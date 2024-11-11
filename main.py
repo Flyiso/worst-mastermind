f2a30b9e86a239bd5603d341495638c478a141ef
Flyiso <dexterpetterssonspang@gmail.com>
2024-11-11 19:23:25 +0100

    some code for pygame/first version of app added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 """
 This is where the application logic is supposed to run
 """
+import pygame
+from pygame.locals import *
+from components import Spinner, SpinnerButton, Colors, GameStatus, GuessGrid
+
+# THIS IS TO BE REMOVED/MODIFIED WHEN MOVING PROJECT TO KIVY/PHONES
+DISPLAYSURF = pygame.display.set_mode((300,600))
+
+
+for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
